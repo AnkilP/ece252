d996367d0bafa202650e9695b6a03f151cbecc89
AnkilP <a293pate@uwaterloo.ca>
2019-07-16 15:33:38 -0400

    separated curl helper functions

---
+++ lab4/curl_helper.h
@@ -0,0 +1,76 @@
+/*
+ * The code is derived from cURL example and paster.c base code.
+ * The cURL example is at URL:
+ * https://curl.haxx.se/libcurl/c/getinmemory.html
+ * Copyright (C) 1998 - 2018, Daniel Stenberg, <daniel@haxx.se>, et al..
+ *
+ * The xml example code is 
+ * http://www.xmlsoft.org/tutorial/ape.html
+ *
+ * The paster.c code is 
+ * Copyright 2013 Patrick Lam, <p23lam@uwaterloo.ca>.
+ *
+ * Modifications to the code are
+ * Copyright 2018-2019, Yiqing Huang, <yqhuang@uwaterloo.ca>.
+ * 
+ * This software may be freely redistributed under the terms of the X11 license.
+ */
+
+/** 
+ * @file main_wirte_read_cb.c
+ * @brief cURL write call back to save received data in a user defined memory first
+ *        and then write the data to a file for verification purpose.
+ *        cURL header call back extracts data sequence number from header if there is a sequence number.
+ * @see https://curl.haxx.se/libcurl/c/getinmemory.html
+ * @see https://curl.haxx.se/libcurl/using/
+ * @see https://ec.haxx.se/callback-write.html
+ */ 
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <curl/curl.h>
+#include <libxml/HTMLparser.h>
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
+#include <libxml/uri.h>
+
+
+#define SEED_URL "http://ece252-1.uwaterloo.ca/lab4/"
+#define ECE252_HEADER "X-Ece252-Fragment: "
+#define BUF_SIZE 1048576  /* 1024*1024 = 1M */
+#define BUF_INC  524288   /* 1024*512  = 0.5M */
+
+#define CT_PNG  "image/png"
+#define CT_HTML "text/html"
+#define CT_PNG_LEN  9
+#define CT_HTML_LEN 9
+
+#define max(a, b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+
+typedef struct recv_buf2 {
+    char *buf;       /* memory to hold a copy of received data */
+    size_t size;     /* size of valid data in buf in bytes*/
+    size_t max_size; /* max capacity of buf in bytes*/
+    int seq;         /* >=0 sequence number extracted from http header */
+                     /* <0 indicates an invalid seq number */
+} RECV_BUF;
+
+
+htmlDocPtr mem_getdoc(char *buf, int size, const char *url);
+xmlXPathObjectPtr getnodeset (xmlDocPtr doc, xmlChar *xpath);
+int find_http(char *fname, int size, int follow_relative_links, const char *base_url);
+size_t header_cb_curl(char *p_recv, size_t size, size_t nmemb, void *userdata);
+size_t write_cb_curl3(char *p_recv, size_t size, size_t nmemb, void *p_userdata);
+int recv_buf_init(RECV_BUF *ptr, size_t max_size);
+int recv_buf_cleanup(RECV_BUF *ptr);
+void cleanup(CURL *curl, RECV_BUF *ptr);
+int write_file(const char *path, const void *in, size_t len);
+CURL *easy_handle_init(RECV_BUF *ptr, const char *url);
+int process_data(CURL *curl_handle, RECV_BUF *p_recv_buf);
